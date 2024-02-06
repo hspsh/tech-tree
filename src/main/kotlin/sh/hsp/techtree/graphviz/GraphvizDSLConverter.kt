b3f7a54f103883e7261eb0c369b4a8095fd44b7d
dominik <dominik.pasymowski@gmail.com>
2024-02-06 20:04:12 +0100

    GraphvizDSLConverter single node implementation

--- src/main/kotlin/sh/hsp/techtree/graphviz/GraphvizDSLConverter.kt
+++ src/main/kotlin/sh/hsp/techtree/graphviz/GraphvizDSLConverter.kt
@@ -1,9 +1,18 @@
 package sh.hsp.techtree.graphviz
 
 import sh.hsp.techtree.TreeModel
+import sh.hsp.techtree.TreeNode
+import java.util.stream.Collectors
 
 class GraphvizDSLConverter {
-    fun convert(model: TreeModel):String {
-        return "digraph { node "
+    fun convert(model: TreeModel): String {
+        return model.nodes.stream()
+            .map { registerNode(it) }
+            .map { "digraph { $it }" }
+            .collect(Collectors.joining())
+    }
+
+    private fun registerNode(node: TreeNode): String {
+        return node.title
     }
 }
