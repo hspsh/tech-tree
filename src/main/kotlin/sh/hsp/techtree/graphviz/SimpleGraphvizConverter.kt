a0c840847d02d3a6888cfac3355879688e757943
dominik <dominik.pasymowski@gmail.com>
2024-02-06 21:51:10 +0100

    SimpleGraphvizConverter

--- src/main/kotlin/sh/hsp/techtree/graphviz/SimpleGraphvizConverter.kt
+++ src/main/kotlin/sh/hsp/techtree/graphviz/SimpleGraphvizConverter.kt
@@ -3,10 +3,12 @@ package sh.hsp.techtree.graphviz
 import sh.hsp.techtree.GraphvizConverter
 import sh.hsp.techtree.TreeModel
 
-class SimpleGraphvizConverter(private val dslConverter: DSLConverter) : GraphvizConverter {
+class SimpleGraphvizConverter(
+    private val dslConverter: DSLConverter,
+    private val commandRunner: GraphvizCommandRunner
+) : GraphvizConverter {
     override fun convert(model: TreeModel, outputPath: String) {
         val dsl = dslConverter.convert(model)
-        // todo run dsl
-        TODO("Not yet implemented")
+        commandRunner.run(dsl)
     }
 }
