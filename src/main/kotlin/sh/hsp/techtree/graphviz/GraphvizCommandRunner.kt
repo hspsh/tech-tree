c32d3df8d46fc20644ae8ac8038338754f13015c
dominik <dominik.pasymowski@gmail.com>
2024-02-06 21:50:02 +0100

    GraphvizCommandRunner tests

---
+++ src/main/kotlin/sh/hsp/techtree/graphviz/GraphvizCommandRunner.kt
@@ -0,0 +1,8 @@
+package sh.hsp.techtree.graphviz
+class GraphvizCommandRunnerException(message: String): RuntimeException(message)
+
+class GraphvizCommandRunner {
+    fun run(dsl: String) {
+    }
+
+}
