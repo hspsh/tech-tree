7de549a2c57cdb1745f3fd1cfbdf8237a9007154
wilin <wilinskwiktor27@gmail.com>
2024-02-25 13:40:24 +0100

    Tree Model validator

--- src/main/kotlin/sh/hsp/techtree/FileSystemYamlReader.kt
+++ src/main/kotlin/sh/hsp/techtree/FileSystemYamlReader.kt
@@ -1,69 +1,17 @@
 package sh.hsp.techtree
 
+import sh.hsp.techtree.TreeModelValidator.Companion.validate
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
-import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import java.io.File
-import java.io.IOException
 import java.net.URL
 
-class FileSystemYamlReader : YamlReader {
+class FileSystemYamlReader(private val mapper: ObjectMapper =
+    ObjectMapper(YAMLFactory()).registerKotlinModule()) : YamlReader {
     override fun readToModel(resource: URL): TreeModel {
-        val mapper = ObjectMapper(YAMLFactory()).registerKotlinModule()
-        val file = File(resource.toURI())
-        val yamlData = file.readText()
-        val treeModel: TreeModel = mapper.readValue(yamlData)
-
-        validateTreeModel(treeModel)
-
-        return treeModel
-    }
-
-    private fun validateTreeModel(treeModel: TreeModel) {
-        val titleList = treeModel.nodes.drop(1).map { it.title }
-        val requiresList = treeModel.nodes.flatMap { it.requires.orEmpty() }
-
-        if (titleList != requiresList) {
-            throw YamlReaderException(generateExceptionMessage(titleList, requiresList, treeModel))
-        }
-    }
-
-    private fun generateExceptionMessage(titleList: List<ID>, requiresList: List<ID>, treeModel: TreeModel): String {
-        val mismatchDescriptions = mutableListOf<String>()
-        mismatchDescriptions.add(findTitlesNotRequiredByAnyone(titleList, requiresList))
-        mismatchDescriptions.add(findRequiredNodesNotFound(titleList, requiresList, treeModel))
-
-        val nonEmptyDescriptions = mismatchDescriptions.filterNot { it.isEmpty() }
-
-        return "Validation failed, because: \n${nonEmptyDescriptions.joinToString("\n")}"
-    }
-
-    private fun findTitlesNotRequiredByAnyone(titleList: List<ID>, requiresList: List<ID>): String {
-        val titlesNotRequired = titleList.filterNot { it in requiresList }
-
-        return titlesNotRequired.joinToString("\n") { "$it was found and is not required by any previous node" }
-    }
-
-    private fun findRequiredNodesNotFound(titleList: List<ID>, requiresList: List<ID>, treeModel: TreeModel): String {
-        val errorMessage = StringBuilder()
-        for (require in requiresList) {
-            if (require !in titleList) {
-                findRequireParentTitle(treeModel, errorMessage, require)
-            }
-        }
-        return errorMessage.toString()
+        return File(resource.toURI())
+            .let { mapper.readValue(it, TreeModel::class.java) }
+            .also { validate(it) }
     }
 }
-
-private fun findRequireParentTitle(treeModel: TreeModel, errorMessage: StringBuilder, require: ID) {
-    for (node in treeModel.nodes) {
-        node.requires?.forEach { requiredId ->
-            if (requiredId == require) {
-                errorMessage.append("$require was not found and is required by ${node.title}\n")
-            }
-        }
-    }
-}
-
-class YamlReaderException(message: String) : IOException(message)
